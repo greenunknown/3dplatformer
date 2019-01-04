b4493c8004c4e3b88897729f60fcbc33af324348
greenunknown <greenunknown0@gmail.com>
2019-01-03 20:46:41 -0800

    Brandon: Working on moving player.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 200f;
+    public float sidewaysForce = 200f;
+    public float jumpForce = 200f;
+
+    // FixedUpdate is called once per frame for consistent intervals
+    void FixedUpdate()
+    {
+        // Forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);    
+    }
+}
