fa0a1434f504fe65491dc7fc2f1e30e2efbf097b
greenunknown <greenunknown0@gmail.com>
2019-01-03 21:04:40 -0800

    Brandon: Add player movement up, down, left, right.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,14 +6,35 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
 
-    public float forwardForce = 200f;
-    public float sidewaysForce = 200f;
-    public float jumpForce = 200f;
+    public float forwardForce = 1000f;
+    public float sidewaysForce = 1000f;
+    public float jumpForce = 1000f;
 
     // FixedUpdate is called once per frame for consistent intervals
     void FixedUpdate()
     {
-        // Forward force
-        rb.AddForce(0, 0, forwardForce * Time.deltaTime);    
+        // Forward
+        if(Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);   
+        }
+        
+        // Left
+        if(Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+         
+        // Right
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        // Back
+        if(Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
+        }
     }
 }
