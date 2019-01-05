82c368ec93a1803404b6eed1c0434c992384e9e6
redunknown <>
2019-01-04 19:56:18 -0800

    fixed issues with EnemyCollision.cs file.

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 10f;
+    
+ 
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0 , forwardForce * Time.deltaTime);
+        
+    }
+}
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyController : MonoBehaviour
+{
+    public float lookRadius = 30f;
+    Transform target;
+    NavMeshAgent agent;
+    
+   
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+  
+    void Update()
+    {
+        
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
