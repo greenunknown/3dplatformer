8d8197977a83ce4dceba97d4757637a0af5e64fb
redunknown <>
2019-01-04 17:06:53 -0800

    Added a score and collision on coins

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+
+public class PlayerCollision : MonoBehaviour
+{  
+    public int PlayerScore = 0;
+    public UnityEngine.Object Coin;
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Coin")
+        {
+           
+            PlayerScore += 1;
+        }
+    }
+}
